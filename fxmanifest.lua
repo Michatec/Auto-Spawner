7b4c9c8571594a033a9f41f4dc1b6bcad50dc6b9
Michachatz <121869403+Michatec@users.noreply.github.com>
2025-04-04 15:33:02 +0200

    Update 1.2 Version Manifest

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'cerulean'
 game 'gta5'
 
 author 'Michachatz'   
-version '1.1'    
+version '1.2'    
 description 'Car Spawner Script: Spawnen und Löschen von Fahrzeugen.'
 
 client_script 'car_spawner.lua'
